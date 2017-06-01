738a19d5fb9adc9265403f7ee7f5aed26d6efcac
Daniel Hammond <danielrhammond@gmail.com>
2017-06-01 08:03:03 -0700

    Init

---
+++ Paste/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Paste
+//
+//  Created by Daniel Hammond on 6/1/17.
+//  Copyright © 2017 Daniel Hammond. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
